f071749d723abb70f4f9509211a51182abe69aff
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-05-13 17:00:04 +0100

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,3 +1,23 @@
+void handle_swap(t_node **stack_a, t_node **stack_b, const char *operation)
+{
+  if (operation[0] == 's' && operation[1] == 'a' && operation[2] == '\0')
+  {
+    swap(stack_a);
+    write(1, "sa\n", 3);
+  }
+  else if (operation[0] == 's' && operation[1] == 'b' && operation[2] == '\0')
+  {
+    swap(stack_b);
+    write(1, "sb\n", 3);
+  }
+  else if (operation[0] == 's' && operation[1] == 's' && operation[2] == '\0')
+  {
+    swap(stack_a);
+    swap(stack_b);
+    write(1, "ss\n", 3);
+  }
+}
+
 void swap(t_node **stack)
 {
   if (!*stack || !(*stack)->next)
