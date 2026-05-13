f73c0943aaf13ced54a27c217ec9be54638a2118
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-05-13 17:55:01 +0100

    Update disorder.c

--- disorder.c
+++ disorder.c
@@ -1,13 +1,27 @@
-/*void swap(t_node **stack)
-{
-  if (!*stack || !(*stack)->next)
-    return;int temp;
-  temp = (*stack)->value;
-  (*stack)->value = (*stack)->next->value;
-  (*stack)->next->value = temp;
-}*/
-
 float  find_disorder(t_node **stack)
 {
-  if (!)
+  size_t  mistakes;
+  size_t  pairs;
+  t_node  *pair_pos0;
+  t_node  *pair_pos1;
+
+  if (!stack || !*stack || !(*stack)->next)
+    return (0.0f);
+
+  mistakes = 0;
+  pairs = 0;
+  pair_pos0 = *stack;
+  while (pair_pos0 != NULL)
+  {
+    pair_pos1 = pair_pos0->next;
+    while (pair_pos1 != NULL)
+    {
+      pairs++;
+      if (pair_pos0->content > pair_pos1->content)
+        mistakes++;
+      pair_pos1 = pair_pos1->next;
+    }
+    pair_pos0 = pair_pos0->next;
+  }
+  return ((float)mistakes / pairs);
 }
