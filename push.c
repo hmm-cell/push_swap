248273a3cba4e273e84f2ddb7a50b0869e8a352b
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-05-19 18:15:15 +0100

    Update push.c

--- push.c
+++ push.c
@@ -1,3 +1,17 @@
+void	handle_push(t_node **stack_a, t_node **stack_b, const char *operation)
+{
+	if (operation[0] == 'p' && operation[1] == 'a' && operation[2] == '\0')
+	{
+		push(stack_b, stack_a);
+		ft_printf("pa\n");
+	}
+	else if (operation[0] == 'p' && operation[1] == 'b' && operation[2] == '\0')
+	{
+		push(stack_a, stack_b);
+		ft_printf("pb\n");
+	}
+}
+
 void push(t_node **stack_1, t_node **stack_2)
 {
     t_node *node_to_move;
