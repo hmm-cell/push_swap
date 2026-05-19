b5d0f7c7250fbab9937611adff6f571194d9ee1e
hmm-cell <75536076+hmm-cell@users.noreply.github.com>
2026-05-19 17:57:53 +0100

    Create find_minimum_pos.c

---
+++ find_minimum_pos.c
@@ -0,0 +1,26 @@
+int  find_minimum_pos(t_node *stack_a)
+{
+  t_node  *ptr;
+  int  min_pos;
+  int  min;
+  int current_pos;
+
+  if (!stack_a)
+    return (0);
+  ptr = stack_a;
+  min_pos = 0;
+  current_pos = 0;
+  min = stack_a->content;
+
+  while (current != NULL)
+  {
+    if (current->content < min)
+    {
+      min = current->content;
+      min_pos = current_pos;
+    }
+    current_pos++;
+    current = current->next;
+  }
+  return (min_pos);
+}
